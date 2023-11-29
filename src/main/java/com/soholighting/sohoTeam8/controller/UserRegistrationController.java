01d26fdac2cf58488b5608930d85f274e4471557
Enoch Ribin Chandrasekaran <chandrasekarane@cardiff.ac.uk>
2023-11-29 16:24:39 +0000

    Added Html

--- src/main/java/com/soholighting/sohoTeam8/controller/UserRegistrationController.java
+++ src/main/java/com/soholighting/sohoTeam8/controller/UserRegistrationController.java
@@ -1,8 +1,24 @@
 package com.soholighting.sohoTeam8.controller;
 
+import com.soholighting.sohoTeam8.service.UserRegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * @author Enoch Ribin 23089855
  */
+
+@RestController
 public class UserRegistrationController {
+
+    @Autowired
+    private UserRegistrationService userRegistrationService;
+
+    @GetMapping("/joinUs")
+    public ModelAndView registerUser() {
+        return new ModelAndView("registerUser");
+    }
     
 }
--- src/main/java/com/soholighting/sohoTeam8/service/UserRegistrationService.java
+++ src/main/java/com/soholighting/sohoTeam8/service/UserRegistrationService.java
@@ -1,8 +1,11 @@
 package com.soholighting.sohoTeam8.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @author Enoch Ribin 23089855
  */
+@Service
 public class UserRegistrationService {
 
 }
