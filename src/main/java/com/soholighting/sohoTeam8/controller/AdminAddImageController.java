757c4dd79852c036f3d209864ad59d0e6e4e2441
Enoch Ribin Chandrasekaran <chandrasekarane@cardiff.ac.uk>
2023-11-30 00:16:13 +0000

    Added files

---
+++ src/main/java/com/soholighting/sohoTeam8/controller/AdminAddImageController.java
@@ -0,0 +1,11 @@
+package com.soholighting.sohoTeam8.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Enoch Ribin 23089855
+ */
+@RestController
+public class AdminAddImageController {
+
+}
