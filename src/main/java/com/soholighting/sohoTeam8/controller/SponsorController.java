e6d6aa550f84316b63ba0aeacf085b70241d2cad
YinShiJie <YinS8@cardiff.ac.uk>
2023-12-08 18:08:57 +0000

    success link database to code

--- src/main/java/com/soholighting/sohoTeam8/controller/SponsorController.java
+++ src/main/java/com/soholighting/sohoTeam8/controller/SponsorController.java
@@ -2,6 +2,7 @@ package com.soholighting.sohoTeam8.controller;
 
 import com.soholighting.sohoTeam8.model.SpecialThanks;
 import com.soholighting.sohoTeam8.model.Sponsors;
+import com.soholighting.sohoTeam8.service.SpecialThanksService;
 import com.soholighting.sohoTeam8.service.SponsorsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,15 +16,17 @@ import java.util.List;
 public class SponsorController {
     @Autowired
     private SponsorsService sponsorsService;
+    @Autowired
+    private SpecialThanksService specialThanksService;
     @GetMapping("/sponsors")
     public String showSponsors(Model model) {
         //get from database later
         // example data
         List<Sponsors> sponsors = sponsorsService.getAllSponsors();
-       // List<SpecialThanks> specialThanksList = getSpecialThanks();
+        List<SpecialThanks> specialThanks= specialThanksService.getAllSpecialThanks();
         // data to model
         model.addAttribute("sponsors", sponsors);
-       // model.addAttribute("specialThanksList", specialThanksList);
+       model.addAttribute("specialThanks", specialThanks);
         // return
         return "sponsors";
     }
--- src/main/java/com/soholighting/sohoTeam8/model/SpecialThanks.java
+++ src/main/java/com/soholighting/sohoTeam8/model/SpecialThanks.java
@@ -6,11 +6,12 @@ public class SpecialThanks {
     private String thanks_logo;
     private int thanks_id;
 
-    public SpecialThanks(String name, String URL, String thanks_logo, int thanks_id) {
+    public SpecialThanks(String name, String URL, String thanks_logo) {
+        this.thanks_id = thanks_id;
         this.name = name;
         this.URL = URL;
         this.thanks_logo = thanks_logo;
-        this.thanks_id = thanks_id;
+
     }
 
     public String getName() {
