b0d9b90019e9b1940d5e5482082685ae568094c6
YinShiJie <YinS8@cardiff.ac.uk>
2023-12-12 18:01:16 +0000

    some comment and remove useless button part

--- src/main/java/com/soholighting/sohoTeam8/controller/SponsorController.java
+++ src/main/java/com/soholighting/sohoTeam8/controller/SponsorController.java
@@ -18,13 +18,12 @@ import java.util.List;
 @Controller
 public class SponsorController {
     @Autowired
-    private SponsorsService sponsorsService;
+    private SponsorsService sponsorsService;//service
     @Autowired
     private SpecialThanksService specialThanksService;
-    @GetMapping("/sponsors")
+    @GetMapping("/sponsors")//get
     public String showSponsors(Model model) {
         //get from database later
-        // example data
         List<Sponsors> sponsors = sponsorsService.getAllSponsors();
         List<SpecialThanks> specialThanks= specialThanksService.getAllSpecialThanks();
         // data to model
@@ -36,10 +35,11 @@ public class SponsorController {
     @GetMapping("/fetch-sponsorship-amounts")
     @ResponseBody
     public int[] fetchSponsorshipAmounts() {
-        // 返回一个整型数组，或者将你的数据转换成 int[] 类型
+        // return a list of sponsorship amounts
         return new int[]{62000, 43000, 60000};
     }
     static class SponsorshipAmountResponse {
+        // sponsorship amounts
         private int[] amounts;
 
         public SponsorshipAmountResponse(int[] amounts) {
@@ -56,7 +56,7 @@ public class SponsorController {
     }
     private final SponsorRepository sponsorRepository;
     private static final Logger logger = LoggerFactory.getLogger(SponsorController.class);
-
+    //logger
     public SponsorController(SponsorRepository sponsorRepository) {
         this.sponsorRepository = sponsorRepository;
     }
@@ -64,16 +64,15 @@ public class SponsorController {
     @GetMapping("/insertSponsor")
     public String showInsertSponsorForm(Model model) {
         logger.info("Fetching all sponsors...");
-        List<Sponsors> allSponsors = sponsorRepository.findAllSponsors();
-        logger.info("Fetched {} sponsors", allSponsors.size());
-        model.addAttribute("newSponsor", new Sponsors());
+        List<Sponsors> allSponsors = sponsorRepository.findAllSponsors();//show
+        model.addAttribute("newSponsor", new Sponsors());// the model object used to pass model data
         model.addAttribute("allSponsors", allSponsors);
         return "insertSponsor";
     }
 
     @PostMapping("/insertSponsor")
     public String insertSponsor(@ModelAttribute Sponsors newSponsor) {
-        sponsorRepository.insertSponsor(newSponsor);
+        sponsorRepository.insertSponsor(newSponsor);//insert
         return "redirect:/sponsors";
     }
     @PostMapping("/removeSponsor")
